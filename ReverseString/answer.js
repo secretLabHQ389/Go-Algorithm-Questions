ffb40ab0ccc424e675ad39c5802357b2acee26a9
secretLabHQ389 <tech_solutions289@protonmail.com>
2022-08-07 20:23:31 +1000

    Reducer

--- ReverseString/answer.js
+++ ReverseString/answer.js
@@ -1,4 +1,9 @@
 function reverse(str) {
   return str.split('').reverse().join('')
 }
-module.exports = reverse;
+module.exports = reverse
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => character + reversed, '')
+}
+module.exports = reverse
